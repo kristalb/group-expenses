f56ccc85c21cc15ec698657e05529deaa754e7b1
Enrico Bianco <enricob@gmail.com>
2009-02-12 02:18:06 -0500

    basic validations added to items model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+  
+  validates_presence_of :title, :message => "can't be blank"
+  validates_presence_of :amount, :message => "must be entered"
+  validates_numericality_of :amount, :message => "must be numerical (e.g. 100.00)"
 end
