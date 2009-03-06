ab434a3fbbacb7c438c8b2274008c9dee294ec36
Enrico Bianco <enricob@gmail.com>
2009-03-05 22:24:56 -0500

    removed customized tasks from config/deploy.rb because they are now in Capfile.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,15 +14,3 @@ set :deploy_via, :copy
 set :scm, :git
 
 server "nodoka", :app, :web, :db, :primary => true
-
-namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-end
